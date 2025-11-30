da53bb8ba04291e086c0537aa8578d05a6feadde
BubbleFish <daforsastudia@gmail.com>
2025-11-30 13:18:26 +0300

    feat: Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import Main from '$lib/components/layout/main.svelte';
+</script>
+
+<Main>
+	<h1 class="--font-rubik">{page.status}</h1>
+	<span>{page.error?.message}</span>
+</Main>
+
+<style>
+	h1 {
+		font-size: 6rem;
+	}
+
+	span {
+		font-size: 1rem;
+	}
+</style>
